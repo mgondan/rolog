29c394e9fec2c8d821b501a66854acb366984225
mgondan <Matthias.Gondan@psy.ku.dk>
2021-06-01 17:51:02 +0200

    Update rologpp.R

--- R/rologpp.R
+++ R/rologpp.R
@@ -1,29 +1,29 @@
 .onLoad = function(libname, pkgname)
 {
-	rologpp_init(libname, pkgname, commandArgs()[1])
+	rolog_init(libname, pkgname, commandArgs()[1])
 }
 
-rologpp_init = function(libname, pkgname, argv1)
+rolog_init = function(libname, pkgname, argv1)
 {
 	init(commandArgs()[1])
 }
 
-rologpp_done = function()
+rolog_done = function()
 {
   done()
 }
 
-rologpp_consult = function(fname='pl/likes.pl')
+rolog_consult = function(fname='pl/likes.pl')
 {
   consult(fname)
 }
 
-rologpp_call = function(call = quote(consult('pl/likes')))
+rolog_call = function(call = quote(consult('pl/likes')))
 {
   call(call)
 }
 
-rologpp_findall = function(predicate=quote(likes(sam)))
+rolog_findall = function(predicate=quote(likes(sam)))
 {
   l = as.list(findall(predicate))
   l[length(l):1]
