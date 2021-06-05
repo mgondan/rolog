d1a78a159a148f77d79ceb97db628f1ef26d1cc0
unknown <Matthias.Gondan@psy.ku.dk>
2021-06-05 20:18:05 +0200

    123

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,22 +2,22 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 init_ <- function(argv0) {
-    .Call(`_rolog_init_`, argv0)
+    .Call('_rolog_init_', PACKAGE = 'rolog', argv0)
 }
 
 done_ <- function() {
-    .Call(`_rolog_done_`)
+    .Call('_rolog_done_', PACKAGE = 'rolog')
 }
 
 consult_ <- function(files) {
-    .Call(`_rolog_consult_`, files)
+    .Call('_rolog_consult_', PACKAGE = 'rolog', files)
 }
 
 call_ <- function(lang) {
-    .Call(`_rolog_call_`, lang)
+    .Call('_rolog_call_', PACKAGE = 'rolog', lang)
 }
 
 findall_ <- function(lang) {
-    .Call(`_rolog_findall_`, lang)
+    .Call('_rolog_findall_', PACKAGE = 'rolog', lang)
 }
 
