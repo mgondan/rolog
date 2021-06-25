985979654551fb0bd478bea452f7f39345905a01
mgondan <Matthias.Gondan@psy.ku.dk>
2021-06-25 17:39:03 +0200

    Update rolog.cpp

--- src/rolog.cpp
+++ src/rolog.cpp
@@ -4,6 +4,7 @@ using namespace Rcpp ;
 
 // PlEngine* pl = NULL ;
 int pl = 0 ;
+char *av[2];
 
 // [[Rcpp::export]]
 LogicalVector init_(String argv0)
@@ -17,11 +18,13 @@ LogicalVector init_(String argv0)
   Rcerr << "rolog_init: starting PlEngine(\"" << const_cast<char*>(argv0.get_cstring()) << "\")" << std::endl ;
   // pl = new PlEngine(const_cast<char*>(argv0.get_cstring())) ;
   
-    int ac = 0;
-    char **av = (char **)malloc(sizeof(char *) * 2);
-    av[ac++] = const_cast<char*>(argv0.get_cstring()) ;
+  int ac = 0;
+
+  av[ac++] = const_cast<char*>(argv0.get_cstring());
+  av[ac]   = NULL;
+  
     Rcerr << "rolog_init: initialize with " << av[0] << std::endl ;      
-    int ret = PL_initialise(1, av) ;
+    int ret = PL_initialise(ac, av) ;
     if(!ret)
     {
       Rcerr << "rolog_init: failed initialize, return value " << ret << std::endl ;      
