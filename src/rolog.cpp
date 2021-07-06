afdbe30631c99f188718f841fa95fdcfb95366e7
mgondan <Matthias.Gondan@psy.ku.dk>
2021-07-06 20:14:35 +0200

    Update rolog.cpp

--- src/rolog.cpp
+++ src/rolog.cpp
@@ -186,8 +186,8 @@ PlTerm r2pl_integer(IntegerVector arg)
 
 PlTerm r2pl_var(SEXP arg, CharacterVector& vars)
 {
-  String name = as<String>(arg) ;
-  if(name[0] != "_")
+  List name = as<List>(arg) ;
+  if(as<String>(name[0]) != "_")
     vars.push_back(name[0]) ;
   
   return PlTerm() ;
