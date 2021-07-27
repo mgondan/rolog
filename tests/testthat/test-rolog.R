ef21e9b51dd8471f847c5f58578639d8431c0c7d
unknown <Matthias.Gondan@psy.ku.dk>
2021-07-27 20:40:19 +0200

    Added a few more unit tests

--- tests/testthat/test-rolog.R
+++ tests/testthat/test-rolog.R
@@ -1,7 +1,23 @@
+test_that("consult works with default arguments", 
+{
+  expect_true(consult())
+})
+
 test_that("once works with default arguments", 
-  {
-    l = list(X=1)
-    attr(l, 'query') = 'member(X, [1.0, 2.0, 3.0])'
-    
-    expect_equal(once(), l)
-  })
+{
+  l = list(X=1)
+  attr(l, 'query') = 'member(X, [1.0, 2.0, 3.0])'
+
+  expect_equal(once(), l)
+})
+
+test_that("findall works with default arguments", 
+{
+  l1 = list(X=1)
+  l2 = list(X=2)
+  l3 = list(X=3)
+  l = list(l1, l2, l3)
+  attr(l, 'query') = 'member(X, [1.0, 2.0, 3.0])'
+  
+  expect_equal(findall(), l)
+})
