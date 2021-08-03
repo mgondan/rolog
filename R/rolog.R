1d69f824711ab76298dc56593f1c46889a2ce5c4
unknown <Matthias.Gondan@psy.ku.dk>
2021-08-03 23:11:28 +0200

    remove doc for argument that does not exist anymore

--- R/rolog.R
+++ R/rolog.R
@@ -203,7 +203,6 @@ once = function(query=call('member', expression(X), list(1, 2, 3)), options=NULL
 #' Invoke a query several times
 #'
 #' @param query an R call
-#' @param portray boolean, add the prolog translation as an attribute
 #' @param options boolean. See rolog_options
 #' @return empty list if the query fails; otherwise, a list of conditions for 
 #'              each solution
