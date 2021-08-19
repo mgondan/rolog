075701232b8061c58c45d5f5bed697d79cb4c62d
mgondan <Matthias.Gondan@psy.ku.dk>
2021-08-19 17:15:00 +0200

    Delete src/linux/src/CMakeFiles directory

--- src/linux/src/CMakeFiles/libswipl.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libswipl.a"
-  "libswipl.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/libswipl.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- src/linux/src/CMakeFiles/vmi-metadata.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vmi-metadata"
-  "vmi-metadata.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vmi-metadata.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
