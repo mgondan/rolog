e04bf41e0f11244e25dca8cb99244a49c0cc696f
mgondan <Matthias.Gondan@psy.ku.dk>
2021-11-07 09:46:00 +0100

    Update rolog.cpp

--- src/rolog.cpp
+++ src/rolog.cpp
@@ -919,12 +919,13 @@ RObject call_(String query)
 // static foreign_t r_eval(PlTermv arg, int arity, void* context)
 PREDICATE(r_eval, 2)
 {
+  throw PlException(PlCompound("r_evalxx", PlTermv(A1, A2))) ;
+
   CharacterVector names ;
   PlTerm vars ;
   List options = List::create(Named("realvec") = "#", Named("boolvec") = "!", Named("charvec") = "$", Named("intvec") = "%", Named("atomize") = false, Named("scalar") = true) ;
     
   RObject Expr = pl2r(A1, names, vars, options) ;
-  throw PlException(PlCompound("r_evalxx", PlTermv(A1, A2))) ;
   RObject Res = Expr ;
   try
   {
