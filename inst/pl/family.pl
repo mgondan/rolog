e74887699750de91ba0c072648241e4d3fedd043
mgondan <Matthias.Gondan@psy.ku.dk>
2021-11-25 13:25:31 +0100

    Add files via upload

---
+++ inst/pl/family.pl
@@ -0,0 +1,39 @@
+female(pam).
+female(liz).
+female(pat).
+female(ann).
+
+male(tom).
+male(bob).
+male(jim).
+
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
+parent(bob, peter).
+parent(peter, jim).
+
+mother(X, Y) :-
+    parent(X, Y),
+    female(X).
+
+brother(X, Y) :-
+    parent(Z, X),
+    parent(Z,Y),
+    male(X),
+    dif(X, Y).
+
+grandparent(X, Y) :-
+    parent(X, Z),
+    parent(Z, Y).
+
+ancestor(X, Z) :-
+    parent(X, Z).
+
+ancestor(X, Z) :-
+    parent(X, Y),
+    ancestor(Y, Z).
+
