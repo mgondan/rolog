5b1d14209fe3c752d01aa12fed18390db7cb4d71
mgondan <Matthias.Gondan@psy.ku.dk>
2021-11-25 19:49:40 +0100

    Update telescope.pl

--- inst/pl/telescope.pl
+++ inst/pl/telescope.pl
@@ -1,28 +1,27 @@
-% A simple English DCG grammar
-% ============================
-
+% Simple grammar with sentences, noun/verb/participle phrases
 s(s(NP,VP)) --> np(NP, Num), vp(VP, Num).
 
 np(NP, Num) --> pn(NP, Num).
-np(np(Det,N), Num) --> det(Det, Num), n(N, Num).
-np(np(Det,N,PP), Num) --> det(Det, Num), n(N, Num), pp(PP).
+np(np(Det, N), Num) --> det(Det, Num), n(N, Num).
+np(np(Det, N, PP), Num) --> det(Det, Num), n(N, Num), pp(PP).
 
-vp(vp(V,NP), Num) --> v(V, Num), np(NP, _).
-vp(vp(V,NP,PP), Num) --> v(V, Num), np(NP, _), pp(PP).
+vp(vp(V, NP), Num) --> v(V, Num), np(NP, _).
+vp(vp(V, NP, PP), Num) --> v(V, Num), np(NP, _), pp(PP).
 
-pp(pp(P,NP)) --> p(P), np(NP, _).
+pp(pp(P, NP)) --> p(P), np(NP, _).
 
-det(det(a), sg) --> [a].
-det(det(the), _) --> [the].
+% Determiners, personal nouns, nouns, verbs and participles
+det(det(a), sg) --> ["a"].
+det(det(the), _) --> ["the"].
 
-pn(pn(john), sg) --> [john].
+pn(pn(john), sg) --> ["john"].
 
-n(n(man), sg) --> [man].
-n(n(men), pl) --> [men].
-n(n(telescope), sg) --> [telescope].
+n(n(man), sg) --> ["man"].
+n(n(men), pl) --> ["men"].
+n(n(telescope), sg) --> ["telescope"].
 
-v(v(sees), sg) --> [sees].
-v(v(see), pl) --> [see].
-v(v(saw), _) --> [saw].
+v(v(sees), sg) --> ["sees"].
+v(v(see), pl) --> ["see"].
+v(v(saw), _) --> ["saw"].
 
-p(p(with)) --> [with].
+p(p(with)) --> ["with"].
