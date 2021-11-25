703f15ea75880359c06cf2ce4bf9ab2f2c0032ef
mgondan <Matthias.Gondan@psy.ku.dk>
2021-11-25 19:12:42 +0100

    Create telescope.pl

---
+++ inst/pl/telescope.pl
@@ -0,0 +1,28 @@
+% A simple English DCG grammar
+% ============================
+
+s(s(NP,VP)) --> np(NP, Num), vp(VP, Num).
+
+np(NP, Num) --> pn(NP, Num).
+np(np(Det,N), Num) --> det(Det, Num), n(N, Num).
+np(np(Det,N,PP), Num) --> det(Det, Num), n(N, Num), pp(PP).
+
+vp(vp(V,NP), Num) --> v(V, Num), np(NP, _).
+vp(vp(V,NP,PP), Num) --> v(V, Num), np(NP, _), pp(PP).
+
+pp(pp(P,NP)) --> p(P), np(NP, _).
+
+det(det(a), sg) --> [a].
+det(det(the), _) --> [the].
+
+pn(pn(john), sg) --> [john].
+
+n(n(man), sg) --> [man].
+n(n(men), pl) --> [men].
+n(n(telescope), sg) --> [telescope].
+
+v(v(sees), sg) --> [sees].
+v(v(see), pl) --> [see].
+v(v(saw), _) --> [saw].
+
+p(p(with)) --> [with].
