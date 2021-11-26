530ab56960331e643058b0bf30c35b6855100f46
mgondan <Matthias.Gondan@psy.ku.dk>
2021-11-26 21:42:59 +0100

    Create myquote.R

---
+++ R/myquote.R
@@ -0,0 +1,30 @@
+myquote = function(x)
+{
+	myq(substitute(x))
+}
+
+myq = function(x)
+{
+	if(is.name(x))
+	{
+		n = substr(as.character(x), 1, 1)
+		
+		# Variable
+		if(n == toupper(n))
+			return(as.expression(x))
+
+		if(n == "_")
+			return(as.expression(x))
+	}
+
+	if(is.call(x))
+	{
+		args = as.list(x)
+		args[-1] = lapply(args[-1], FUN=myq)
+		return(as.call(args))
+	}
+	
+	return(x)
+}
+
+# myquote(dbinom(`_k`, N, pi))
