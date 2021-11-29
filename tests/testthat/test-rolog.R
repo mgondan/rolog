46d294d2cbd1213721cd8776ef2aa8c501e21690
mgondan <Matthias.Gondan@psy.ku.dk>
2021-11-29 11:42:27 +0100

    fixed a bug in a unit test

--- tests/testthat/test-rolog.R
+++ tests/testthat/test-rolog.R
@@ -5,19 +5,22 @@ test_that("consult works with default arguments",
 
 test_that("once works with default arguments", 
 {
-  l = list(X=1)
-  attr(l, 'query') = 'member(X, [1.0, 2.0, 3.0])'
+  l = list(X=as.symbol("a"))
+  attr(l, "query") = "member(X, [a, b, 3, 4.0, true, Y])"
 
   expect_equal(once(), l)
 })
 
 test_that("findall works with default arguments", 
 {
-  l1 = list(X=1)
-  l2 = list(X=2)
-  l3 = list(X=3)
-  l = list(l1, l2, l3)
-  attr(l, 'query') = 'member(X, [1.0, 2.0, 3.0])'
+  l1 = list(X=as.symbol("a"))
+  l2 = list(X="b")
+  l3 = list(X=3L)
+  l4 = list(X=4)
+  l5 = list(X=TRUE)
+  l6 = list(Y=expression(X))
+  l = list(l1, l2, l3, l4, l5, l6)
+  attr(l, "query") = "member(X, [a, b, 3, 4.0, true, Y])"
   
   expect_equal(findall(), l)
 })
