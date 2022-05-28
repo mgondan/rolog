317ee6ef12b9093475f7d773a2d544c8884e078c
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2022-05-28 22:00:48 +0200

    Update rolog.R

--- R/rolog.R
+++ R/rolog.R
@@ -64,6 +64,9 @@
     if(arch == 'aarch64')
       arch <- 'arm64'
     folder <- dir(fp, pattern=arch, full.names=TRUE)
+    if(!length(folder) & arch == "arm64")
+      folder <- dir(fp, pattern="aarch64-linux", full.names=TRUE)	
+
     if(R.version$os == "linux-gnu")
       dyn.unload(file.path(folder, paste("libswipl", .Platform$dynlib.ext, sep="")))
     else
