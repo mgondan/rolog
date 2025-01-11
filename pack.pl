62b215d51e5b5e4a0e031f32336c7905b21bff5b
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2025-01-11 16:01:07 +0100

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -1,4 +1,4 @@
-name(rologpp).
+name(rolog).
 title('Simple interface to R').
-version('0.4').
-author('Matthias Gondan', 'Matthias.Gondan@psy.ku.dk').
+version('0.9.19').
+author('Matthias Gondan', 'Matthias.Gondan-Rochon@uibk.ac.at').
